392d02b82be28b53ad7259cf469d90ce97655a8e
p-karari <kararikelvin2002@gmail.com>
2025-10-01 17:52:22 +0300

    update: client components

---
+++ src/components/test-comment.tsx
@@ -0,0 +1,15 @@
+// import { createContext, useState } from "react";
+
+// const AppContext = createContext();
+
+// export function AppWrapper ({ children }: {
+//     children: React.ReactNode;
+// }) {
+//     let [state, setState] = useState('Hello')
+
+//     return (
+//         <AppContext>
+//             {children}
+//         </AppContext>
+//     )
+// }
