dc9e20d29fc95553f41f0906f3ee83e7c7b74e50
p-karari <kararikelvin2002@gmail.com>
2025-10-07 18:03:36 +0300

    Main dashboard pages components

---
+++ src/app/session-location/page.tsx
@@ -0,0 +1,9 @@
+import LocationSelector from "@/components/locations/LocationSelector";
+
+export default function LocationSelectorPage() {
+    return (
+        <>
+            <LocationSelector />
+        </>
+    )
+}
