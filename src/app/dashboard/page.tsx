e2ed91f9fd0f10196112a85ea6c44fad0fc199f7
p-karari <kararikelvin2002@gmail.com>
2025-10-07 18:02:31 +0300

    Main dashboard pages components

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+// import { DashboardSidebar } from "@/components/layout/DashboardSideBar";
+
+export default function DashboardPage() {
+  return (
+    <div className="flex min-h-screen">
+      {/* Sidebar */}
+        
+     
+    </div>
+  );
+}
