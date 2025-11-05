e828c32e943474e2cb9ec3d181eb35ab7106b9a3
p-karari <kararikelvin2002@gmail.com>
2025-11-05 17:41:02 +0300

    update: Made the default page the login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import LoginForm from "@/components/auth/LoginForm";
 
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      this is the default path page
-    </div>
-  );
+export default function LoginPage() {
+    return (
+        <>
+            <LoginForm />
+        </>
+    );
 }
